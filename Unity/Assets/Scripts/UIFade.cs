00134305f772a16c038f4ecebdb11c12020e9f7e
Daniel Dias <dabadias@proton.me>
2023-02-05 11:06:08 +0000

    Add fade to game title

---
+++ Unity/Assets/Scripts/UIFade.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum FadeType
+{
+    FadeIn,
+    FadeOut,
+    FadeInOut,
+    FadeOutIn
+}
+
+public class UIFade : MonoBehaviour
+{
+    [Tooltip("The UI element you want to fade")]
+    [SerializeField]
+    private MaskableGraphic element;
+
+    [Tooltip("Fade type")]
+    [SerializeField]
+    private FadeType fadeType;
+
+    [Tooltip("Fade time")]
+    [SerializeField]
+    private float fadeTime = 1f;
+
+    private Color color;
+
+    void Start()
+    {
+        color = element.color;
+        switch (fadeType)
+        {
+            case FadeType.FadeIn:
+                StartCoroutine(FadeIn());
+                break;
+            case FadeType.FadeOut:
+                StartCoroutine(FadeOut());
+                break;
+            case FadeType.FadeInOut:
+                StartCoroutine(FadeInOut());
+                break;
+            case FadeType.FadeOutIn:
+                StartCoroutine(FadeOutIn());
+                break;
+        }
+    }
+
+    private IEnumerator FadeOut()
+    {
+        for (float a = fadeTime; a >= 0; a -= Time.deltaTime)
+        {
+            element.color = new Color(color.r, color.g, color.b, a);
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeIn()
+    {
+        for (float a = 0; a <= fadeTime; a += Time.deltaTime)
+        {
+            element.color = new Color(color.r, color.g, color.b, a);
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeInOut()
+    {
+        StartCoroutine(FadeIn());
+        yield return new WaitForSeconds(fadeTime);
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOutIn()
+    {
+        StartCoroutine(FadeOut());
+        yield return new WaitForSeconds(fadeTime);
+        StartCoroutine(FadeIn());
+    }
+}
