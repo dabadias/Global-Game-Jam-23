00f1d47ad0a9b7c882b61ed98ccb17e6671b80c8
Daniel Dias <dabadias@proton.me>
2023-02-05 10:27:15 +0000

    Add win con

---
+++ Unity/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    public int treeConnections;
+    private int _treeConnectionsCurrent = 0;
+
+    public void IncreaseTreeConnections()
+    {
+        _treeConnectionsCurrent++;
+        if (_treeConnectionsCurrent == treeConnections)
+        {
+            // TODO: win
+            Debug.Log("COMPLETE");
+        }
+    }
+}
