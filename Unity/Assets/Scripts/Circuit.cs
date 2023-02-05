e0216d92e3ab2b2ec988400880beffab5ee1f8d2
Daniel Dias <dabadias@proton.me>
2023-02-05 12:08:09 +0000

    Blind player A from circuits

--- Unity/Assets/Scripts/Circuit.cs
+++ Unity/Assets/Scripts/Circuit.cs
@@ -4,14 +4,46 @@ using UnityEngine.Events;
 
 public class Circuit : MonoBehaviour
 {
+    public GameObject blind;
     public GameObject from;
     public GameObject to;
     public int size;
     public UnityEvent onTrigger;
+    public MeshRenderer _lampFrom, _accentFrom, _lampTo, _accentTo;
+    private Color _lampColor, _accentColor;
+    private PlayerSwitcher _playerSw;
+
+    private void Start()
+    {
+        _playerSw = GameObject.FindWithTag("PlayerController").GetComponent<PlayerSwitcher>();
+
+        _lampColor = _lampFrom.material.color;
+        _accentColor = _accentFrom.material.color;
+        PaintCircuit();
+    }
 
     public IEnumerator ActivateCo()
     {
         onTrigger.Invoke();
         yield return 0;
+    }
+
+    public void PaintCircuit()
+    {
+        if (_playerSw.currentPlayer == blind)
+        {
+            Debug.Log(true);
+            _lampFrom.material.color = new Color(255f, 255f, 255f);
+            _accentFrom.material.color = new Color(255f, 255f, 255f);
+            _lampTo.material.color = new Color(255f, 255f, 255f);
+            _accentTo.material.color = new Color(255f, 255f, 255f);
+        }
+        else
+        {
+            _lampFrom.material.color = _lampColor;
+            _accentFrom.material.color = _accentColor;
+            _lampTo.material.color = _lampColor;
+            _accentTo.material.color = _accentColor;
+        }
     }
 }
